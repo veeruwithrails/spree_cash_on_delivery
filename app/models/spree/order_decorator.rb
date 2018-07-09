3c74200f4697be7b8b06bae9da69d7992e78932a
veeruwithrails <veeruwithrails@gmail.com>
2018-07-09 23:18:15 +0530

    cash on delivery method added

---
+++ app/models/spree/order_decorator.rb
@@ -0,0 +1,7 @@
+module Spree
+  Order.class_eval do
+    def cash_on_delivery?
+      payments.where(state: ['checkout', 'pending']).map { |m| m.payment_method.type }.include? "Spree::PaymentMethod::CashOnDelivery"
+    end
+  end
+end
